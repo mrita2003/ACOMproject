20a3718a9c6fb980c08a0a3ee2ad3b948015fd12
mrita2003 <155892452+mrita2003@users.noreply.github.com>
2024-02-28 11:42:18 +0530

    Update ACOMWallet.sol

--- ACOMWallet.sol
+++ ACOMWallet.sol
@@ -1,1 +1,14 @@
+SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AGEMSToken is ERC20 {
+ 
+    constructor() ERC20("AGEMS", "token") {
+        _mint(msg.sender, 9000000000);
+   
+
+    }
+}
 
